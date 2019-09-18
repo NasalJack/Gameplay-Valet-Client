58362fc8a7c4f5f1b8d213cdc736942fb495aad8
Sam Walker <jack.nasal@gmail.com>
2019-09-18 10:08:52 -0400

    added a message about not having games to the my games page if it's empty

--- src/components/MyGamesPage/MyGamesPage.js
+++ src/components/MyGamesPage/MyGamesPage.js
@@ -17,13 +17,17 @@ class MyGamesPage extends React.Component {
           <GamesListEntry userPage={true} key={game_id} id={game_id} title={title} genres={genres} rating={rating} />
       )
     });
+    const noGames = 
+      <p>You haven't added any games to your personal list yet. Go back to the all games page and
+        add a game to have it show up here.
+      </p>
 
     return (
       <div className='MyGamesPage'>
         <header className="banner">
             <h1>My Games</h1>
         </header>
-        {gamesList}
+        {!!gamesList.length ? gamesList : noGames}
       </div>
     )
   }
