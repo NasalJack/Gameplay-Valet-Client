c4090727bc90d08f39daab9f39801b2afe0170f4
Sam Walker <jack.nasal@gmail.com>
2019-09-19 11:26:31 -0400

    abandoned react-pdf and added iframe linking to external website pdf

--- src/components/RulesPage/RulesPage.js
+++ src/components/RulesPage/RulesPage.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Document, Page, pdfjs } from 'react-pdf';
-import Catan from './Catan.pdf'
+// import { Document, Page, pdfjs } from 'react-pdf';
+// import Catan from './Catan.pdf'
 
 import ValetApiService from '../../services/valet-api-service';
 
 import './RulesPage.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 class RulesPage extends React.Component {
 
   state = {
     error: null,
-    numPages: null,
-    pageNumber: 1
+    // numPages: null,
+    // pageNumber: 1
   }
 
   componentDidMount() {
@@ -26,9 +26,9 @@ class RulesPage extends React.Component {
       .catch(res => this.setState({ error: res.error }))
   }
 
-  onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
-  };
+  // onDocumentLoadSuccess = ({ numPages }) => {
+  //   this.setState({ numPages });
+  // };
 
   goToPrevPage = () =>
     this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
@@ -36,7 +36,7 @@ class RulesPage extends React.Component {
     this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
 
   render() {
-    const { pageNumber, numPages } = this.state;
+    // const { pageNumber, numPages } = this.state;
     return (
       <div className='RulesPage'>
         <header className="banner">
@@ -45,15 +45,20 @@ class RulesPage extends React.Component {
           <p>{this.state.error}</p>
         </header>
 
-        <section>
-          {this.props.rules}
+        {/* <section>
           <Document file={Catan} onLoadSuccess={this.onDocumentLoadSuccess} >
             <Page pageNumber={pageNumber} width={document.documentElement.clientWidth}/>
           </Document>
           <button disabled={this.state.pageNumber === 1} onClick={this.goToPrevPage}>{'< Prev'}</button>
           <button disabled={this.state.pageNumber === this.state.numPages} onClick={this.goToNextPage}>{'Next >'}</button>
           <p>Page {pageNumber} of {numPages}</p>
-        </section>
+        </section> */}
+
+        <iframe title="Rules PDF" src={this.props.rules} />
+
+        <button className='pdf-link'>
+          <a target='_blank' rel='noopener noreferrer' href={this.props.rules}>View PDF in new page</a>
+        </button>
 
         <button className='main-page' onClick={this.props.history.goBack}>Main Page</button>
       </div>
